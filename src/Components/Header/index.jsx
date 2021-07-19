fc4409e808609e2abe3e34327437dfe39b49bd56
henriquepolonibr <hpoloni11@gmail.com>
2021-07-19 19:15:26 -0300

    Header created

---
+++ src/Components/Header/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as S from './styles';
+
+const Header = () => (
+  <div>
+    <S.Header>
+      <S.HeaderTitle>Header</S.HeaderTitle>
+    </S.Header>
+  </div>
+);
+
+export default Header;
---
+++ src/Components/Header/styles.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Header = styled.div`
+  display: block;
+  width: 100%;
+  background-color: grey;
+`;
+
+export const HeaderTitle = styled.h2`
+  font-size: 1.375rem;
+`;
