6dc96672d82e77eebc654c2da7a78741880c2135
henriquepolonibr <hpoloni11@gmail.com>
2021-07-23 08:58:51 -0300

    Context api implemented at sidebar component

--- src/Components/SideBar/index.jsx
+++ src/Components/SideBar/index.jsx
@@ -1,42 +1,50 @@
+/* eslint-disable prefer-const */
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, { useContext } from 'react';
 import * as S from './styles';
+import { UserContext } from '../../contexts/component';
 
-const SideBar = () => (
-  <S.SideBar>
-    <S.OrangeButton>
-      Fotos
-      <br />
-      para
-      <br />
-      Imobiliária
-      <br />
-    </S.OrangeButton>
-    <S.List>
-      Quartos
-      <S.ListItem>Suíte Casal</S.ListItem>
-      <S.ListItem>Suíte Solteiro 1</S.ListItem>
-      <S.ListItem>Suíte Solteiro 2</S.ListItem>
-      <S.ListItem>Suíte Solteiro 3</S.ListItem>
-      <S.ListItem>Suíte Hóspedes</S.ListItem>
-    </S.List>
-    <S.List>Escritório</S.List>
-    <S.List>
-      Salas
-      <S.ListItem>Sala de Estar</S.ListItem>
-      <S.ListItem>Sala de Televisão</S.ListItem>
-      <S.ListItem>Sala de Jantar</S.ListItem>
-    </S.List>
-    <S.List>Cozinha</S.List>
-    <S.List>Garagem</S.List>
-    <S.List>Varanda</S.List>
-    <S.List>Lavabo</S.List>
-    <S.List>Salão</S.List>
-    <S.List>Suíte Quarto Empregado</S.List>
-    <S.List>Lavanderia</S.List>
-    <S.List>Churrasqueira</S.List>
-    <S.List>Piscina</S.List>
-  </S.SideBar>
-);
+const SideBar = () => {
+  let { componentName, setComponentName } = useContext(UserContext);
+  const handleId = (e) => {
+    setComponentName(e.target.id);
+  };
+  return (
+    <S.SideBar>
+      <S.OrangeButton>
+        Fotos
+        <br />
+        para
+        <br />
+        Imobiliária
+        <br />
+      </S.OrangeButton>
+      <S.List>
+        Quartos
+        <S.ListItem id="suiteCasal" onClick={handleId}>Suíte Casal</S.ListItem>
+        <S.ListItem id="suiteSolteiro1" onClick={handleId}>Suíte Solteiro 1</S.ListItem>
+        <S.ListItem>Suíte Solteiro 2</S.ListItem>
+        <S.ListItem>Suíte Solteiro 3</S.ListItem>
+        <S.ListItem>Suíte Hóspedes</S.ListItem>
+      </S.List>
+      <S.List>Escritório</S.List>
+      <S.List>
+        Salas
+        <S.ListItem>Sala de Estar</S.ListItem>
+        <S.ListItem>Sala de Televisão</S.ListItem>
+        <S.ListItem>Sala de Jantar</S.ListItem>
+      </S.List>
+      <S.List>Cozinha</S.List>
+      <S.List>Garagem</S.List>
+      <S.List>Varanda</S.List>
+      <S.List>Lavabo</S.List>
+      <S.List>Salão</S.List>
+      <S.List>Suíte Quarto Empregado</S.List>
+      <S.List>Lavanderia</S.List>
+      <S.List>Churrasqueira</S.List>
+      <S.List>Piscina</S.List>
+    </S.SideBar>
+  );
+};
 
 export default SideBar;
--- src/contexts/component.js
+++ src/contexts/component.js
@@ -1,15 +1,13 @@
 /* eslint-disable no-undef */
 /* eslint-disable react/prop-types */
-/* eslint-disable no-unused-vars */
 import React, { useState, createContext } from 'react';
-import SuiteSolteiro1 from '../Components/SuiteSolteiro1';
 
 export const UserContext = createContext({});
 
 function UserProvider({ children }) {
   const [componentName, setComponentName] = useState('suiteCasal');
   return (
-    <UserContext.Provider value={{ componentName }}>
+    <UserContext.Provider value={{ componentName, setComponentName }}>
       {children}
     </UserContext.Provider>
   );
